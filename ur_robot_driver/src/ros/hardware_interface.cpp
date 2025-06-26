0a629a3a75b4322d8d5ace8416551df9984210f2
sherry <sherry@chefrobotics.ai>
2025-06-26 15:25:34 -0700

    [TEST ONLY] remove target_pos, target_vel, target_accel, actual_pos, actual_vel

--- ur_robot_driver/src/ros/hardware_interface.cpp
+++ ur_robot_driver/src/ros/hardware_interface.cpp
@@ -812,11 +812,11 @@ void HardwareInterface::publishJointStateExtended(const ros::Time& timestamp)
   {
     const size_t N = joint_names_.size();
     msg.names = joint_names_;
-    msg.target_positions.reserve(N);
-    msg.actual_positions.reserve(N);
-    msg.target_velocities.reserve(N);
-    msg.actual_velocities.reserve(N);
-    msg.target_accelerations.reserve(N);
+    // msg.target_positions.reserve(N);
+    // msg.actual_positions.reserve(N);
+    // msg.target_velocities.reserve(N);
+    // msg.actual_velocities.reserve(N);
+    // msg.target_accelerations.reserve(N);
     msg.target_currents.reserve(N);
     msg.actual_currents.reserve(N);
     msg.actual_current_windows.reserve(N);
@@ -828,22 +828,22 @@ void HardwareInterface::publishJointStateExtended(const ros::Time& timestamp)
     first_time = false;
   }
 
-  if (has_target_joint_positions_)
-    msg.target_positions.assign(target_joint_positions_.begin(), target_joint_positions_.end());
-  else
-    msg.target_positions.clear();
-  msg.actual_positions.assign(joint_positions_.begin(), joint_positions_.end());
+  // if (has_target_joint_positions_)
+  //   msg.target_positions.assign(target_joint_positions_.begin(), target_joint_positions_.end());
+  // else
+  //   msg.target_positions.clear();
+  // msg.actual_positions.assign(joint_positions_.begin(), joint_positions_.end());
 
-  if (has_target_joint_velocities_)
-    msg.target_velocities.assign(target_joint_velocities_.begin(), target_joint_velocities_.end());
-  else
-    msg.target_velocities.clear();
-  msg.actual_velocities.assign(joint_velocities_.begin(), joint_velocities_.end());
+  // if (has_target_joint_velocities_)
+  //   msg.target_velocities.assign(target_joint_velocities_.begin(), target_joint_velocities_.end());
+  // else
+  //   msg.target_velocities.clear();
+  // msg.actual_velocities.assign(joint_velocities_.begin(), joint_velocities_.end());
 
-  if (has_target_joint_accelerations_)
-    msg.target_accelerations.assign(target_joint_accelerations_.begin(), target_joint_accelerations_.end());
-  else
-    msg.target_accelerations.clear();
+  // if (has_target_joint_accelerations_)
+  //   msg.target_accelerations.assign(target_joint_accelerations_.begin(), target_joint_accelerations_.end());
+  // else
+  //   msg.target_accelerations.clear();
 
   if (has_target_joint_efforts_)
     msg.target_currents.assign(target_joint_efforts_.begin(), target_joint_efforts_.end());
